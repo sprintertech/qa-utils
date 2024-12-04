781104a0009169e1989a7fc50e4beb2a2bb2e969
LiviuD <livium.damian@gmail.com>
2024-12-04 15:20:29 +0200

    updated dev config

--- src/Sygma_Tests/shared/config.ts
+++ src/Sygma_Tests/shared/config.ts
@@ -9,7 +9,7 @@ export async function setupNetworkConfig(): Promise<NetworkConfig> {
   const configPath =
     {
       devnet:
-        "https://chainbridge-assets-stage.s3.us-east-2.amazonaws.com/balance-config-dev.json",
+        "https://chainbridge-assets-stage.s3.us-east-2.amazonaws.com/shared-config-dev.json",
       testnet:
         "https://chainbridge-assets-stage.s3.us-east-2.amazonaws.com/shared-config-test.json",
       mainnet:
